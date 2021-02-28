f14688023877a7d8145a1f95dd68c4d3fb053e20
joaco <patricio.jlg@gmail.com>
2021-02-28 01:24:11 -0300

    Where we Go!

--- src/main-component/App/App.js
+++ src/main-component/App/App.js
@@ -2,9 +2,7 @@ import React from 'react';
 import AllRoute from '../router'
 import './App.css';
 import '../../Assets/Css/Responsive.css'
-import { routesCreater as createRoutes } from 'react-router-sitemap';
-import { routesParser as parseRoutes } from 'react-router-sitemap';
-import { Route } from 'react-router-dom';
+
 
 const App = () => { 
 
